4b67b6e1de886099f90479648316917f10144599
ETG1 <ETG1@FNXWSF2-SHEL>
2022-08-26 18:05:27 +0530

    26/08/2022 work success

---
+++ Batch1-DET-2022/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class Circle
+    {
+        double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public double area()
+        {
+            return Math.PI * radius * radius;
+        }
+
+    }
+
+}
---
+++ Batch1-DET-2022/TestEmp.cs
@@ -0,0 +1,18 @@
+﻿using Batch1_DET_2022;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch_DET_2022
+{
+    internal class TestEmp
+    {
+        public static void Main()
+        {
+            Emp e = new Emp(5050, "Rutuja", new DateOnly(2020, 8, 1));
+            Console.WriteLine($"Years of Experience{e.GetYearsofExp()}");
+        }
+    }
+}
